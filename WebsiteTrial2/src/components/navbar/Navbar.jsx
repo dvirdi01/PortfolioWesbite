45fce3583c9cab9a492b8c7c7ed62bd77efcb6ae
dvirdi01 <virdidivjot@gmail.com>
2024-05-03 16:36:40 -0700

    Added navbar html
    added navbar html

--- WebsiteTrial2/src/components/navbar/Navbar.jsx
+++ WebsiteTrial2/src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import styles from "./Navbar.module.css"
 
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+  return <nav className= {styles.navbar}>
+    <a className = {styles.title} href="/">My Portfolio</a> {/* Will take me to main home page when I click on portfolio */}
+    <div className= {styles.menu}> {/* div containing my navbar */}
+        <ul className= {styles.menuItems}>
+            <li> <a href="#about">About Me</a></li>
+            <li> <a href="#experience">Experience</a></li>
+            <li> <a href="#projects">Projects</a></li>
+            <li> <a href="#connect">Connect</a></li>
+        </ul>
+    </div>
+  </nav>;
 }
