52b773eb6d5ebcafdb89a0e06f150f11a3af1727
dvirdi01 <virdidivjot@gmail.com>
2024-05-04 11:55:59 -0700

    installed three.js dependencies

---
+++ WebsiteTrial2/src/components/navbar/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Hero = () => {
+  return <section>
+    <div>
+        <h1>Hi, I'm Divjot</h1>
+        <p>I'm a software developer with three years of experience. Reach out if you'd like to know more</p>
+        <button className = "button1" onclick="window.open('./assets/divjotResumeCoop2.pdf')">Download Resume</button>
+        <a href="mailto:virdidivjot@gmail.com">Get in Touch!</a>
+    </div>
+    
+  </section>;
+}
