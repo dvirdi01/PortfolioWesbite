5e422ab883452ee81e8a64130ef8d662a7e7aaf0
dvirdi01 <virdidivjot@gmail.com>
2024-05-09 23:18:03 -0700

    added photo
    added photo for hero section

--- WebsiteTrial2/src/components/navbar/Hero/Hero.jsx
+++ WebsiteTrial2/src/components/navbar/Hero/Hero.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { AvatarCanvas } from './AvataCanvas';
+import { getImageURL } from '../../../utils';
+import styles from "./Hero.module.css";
 
 export const Hero = () => {
-  return <section>
-    <div>
-        <h1>Hi, I'm Divjot</h1>
-        <p>I'm a software developer with three years of experience. Reach out if you'd like to know more</p>
-        <button className = "button1" onclick="window.open('./assets/divjotResumeCoop2.pdf')">Download Resume</button>
-        <a href="mailto:virdidivjot@gmail.com">Get in Touch!</a>
+  return <section className={styles.container}>
+    <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Divjot</h1>
+        <p className={styles.description}>I'm a software developer with three years of experience. Reach out if you'd like to know more</p>
+        <a href="./assets/divjotResumeCoop2.pdf" target="_blank" className={styles.knowMoreButton}>Download Resume</a>
+        <a href="mailto:virdidivjot@gmail.com" className={styles.knowMoreButton}>Get in Touch!</a>
+        {/* <AvatarCanvas /> */}
+
     </div>
-    
+    <img src={getImageURL("hero/LinkedInHeadshot-modified.png")} alt = "Hero image of me" className={styles.heroimg}/>
+    <div className={styles.topBlur}></div>
+    <div className={styles.bottomBlur}></div>
   </section>;
 }
